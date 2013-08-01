0dc43cd35fb0b51e08613e477d3c7d5b041ebb3f
shannonbit <emmanuelle@emprend.me>
2013-08-01 17:44:44 -0500

    BAR001 | Ligas
    Muestra una liga de texto a cada página del sitio.

--- wordpress/wp-content/themes/zoom1x/footer.php
+++ wordpress/wp-content/themes/zoom1x/footer.php
@@ -2,13 +2,12 @@
 <script type="text/javascript" src="<?php bloginfo( 'template_directory' ); ?>/js/nivo.js"></script>
 <script type="text/javascript" src="<?php bloginfo( 'template_directory' ); ?>/js/galeria.js"></script>
 
-
-
 <!-- inicia footer -->
 <footer>
    <?php get_template_part('derechos'); ?>
 </footer><!-- termina footer -->
 </div><!-- termina imagenes -->
 </div> <!-- termina todo -->
+<?php get_sidebar(); ?>
 </body>
 </html>
--- wordpress/wp-content/themes/zoom1x/js/galeria.js
+++ wordpress/wp-content/themes/zoom1x/js/galeria.js
@@ -3,3 +3,11 @@ function mostrar_grande(id)
 	$('.foto_grande').hide();
 	$('.' + id).show();
 }
+function mostrar_menu()
+{
+	$('.slidingMenu').show();
+}
+function ocultar_menu()
+{
+	$('.slidingMenu').hide();
+}
