4ec41e92b1ecb9782e0b0ce454a07b0b5b1793f5
Jorge Olvera <jorge@emprend.me>
2013-08-06 16:08:12 -0500

    #Versión: 1.2013.08.06

--- wordpress/wp-content/themes/zoom1x/page-contacto.php
+++ wordpress/wp-content/themes/zoom1x/page-contacto.php
@@ -1,37 +1,7 @@
 <!-- Página de Contacto.
+	Crea una página que se llame "Contaco" en el WP.
+	Pon el contenido dentro de WP.
+	Aqui solo manda a llamar el loop.
+	Los estilos se los das con CSS
+	Todo excepto el mapa y el formulario, debe ingresarse desde la página en WP.
 -->
-<?php get_header(); ?>
-<div class="tituloContacto">
-		Contacto
-</div>
-<!--Termina tituloContacto-->
-<div class="contacto">
-	<form class="contactos" metod="post" action="https://dub110.mail.live.com/default.aspx?id=64855">
-		<h3>
-			Nombre:
-		</h3>
-			<INPUT type=text name="nombre" placeholder="Nombre.." />
-		<h3>
-			Correo:
-		</h3>
-			<INPUT  type=text name="correo" placeholder="Correo.." />
-		<h3>
-			Telefono:
-		</h3>
-			<INPUT type=text name="nombre" placeholder="Telefono.." />
-		<h3>
-			Mensaje:
-		</h3>		
-		<textarea placeholder="Mensaje.." class="mensaje" />
-
-		</textarea>
-		<!-- Termina el textarea-->
-		<input class="botonAceptar"type="submit" value="Enviar">
-		<!--Boton para enviar el formulario-->
-		<input class="botonBorrar"type="reset" value="Borrar">
-		<!-- Boton para borrar los datos ingresados-->
-	</form>
-	<!-- fin del form contactos>-->
-</div>
-<!-- aqui termina contacto-->
-<?php get_footer();?>
--- wordpress/wp-content/themes/zoom1x/page-paquetes.php
+++ wordpress/wp-content/themes/zoom1x/page-paquetes.php
@@ -23,40 +23,43 @@
 		</div>
 
 
-
 <!-- termina paquetes -->
 
-					
+					Sesiones fotográficas con mascotas.
+
+					Pregunta por nuestros paquetes fotográficos  para eventos. 
+					También contamos con video y edición.
+
 
 					<div class="terminos">
-						<button id="hidr" class="boton">Terminos y condiciones</button>
+				
+                    <button id="hidr" class="boton">Terminos y condiciones</button>
                   	
-                  			<span>                       
+
+                  	<span>
+                       
                                 <section class="condiciones">
-									<ul>
+						
+					
+										<ul>
 										<li>Las sesiones duran entre 30 minutos y 1 hora 30 minutos, dependiendo del paquete contratado y del número de integrantes por estudio.</li>
+
 										<li>Es recomendable traer objetos personales a la sesión como juguetes preferidos, prendas de ropa cómoda, etc.</li>
+
 										<li>50 por ciento de anticipo y 50 restante a la entrega del paquete. Sólo efectivo.</li>
+
 										<li>La entrega de los paquetes 1 y 2 será una semana después de la sesión. De los paquetes 3 y 4,  en 15 días.</li>
+
+
 										<li>Cargos adicionales:
 										 En caso de requerir maquillaje 
 										 Por sesión en domicilio.
 										 Después de 6 integrantes por sesión, se cobrará 120 por persona extra.</li>
 										 </ul>
+
 									</section>
-					 		</span>
-
-					</div><!-- TERMINA DIV terminos-->
-
-					<div class="extra">
-					<p>Sesiones fotográficas con mascotas.</p>
-
-					Pregunta por nuestros paquetes fotográficos  para eventos. 
-					También contamos con video y edición.
-
-						
-					</div>
-
+					 </span>
+</div>
 
 					
 
