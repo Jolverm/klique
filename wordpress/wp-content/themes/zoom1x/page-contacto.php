6e2b026f554406c5ef081115002cc3f8a5001efd
Jorge Olvera <jorge@emprend.me>
2013-08-12 14:09:04 -0500

    #8 CON001 | #10 CON003  |

--- wordpress/wp-content/themes/zoom1x/page-contacto.php
+++ wordpress/wp-content/themes/zoom1x/page-contacto.php
@@ -1,7 +1,40 @@
-<!-- Página de Contacto.
-	Crea una página que se llame "Contaco" en el WP.
-	Pon el contenido dentro de WP.
-	Aqui solo manda a llamar el loop.
-	Los estilos se los das con CSS
-	Todo excepto el mapa y el formulario, debe ingresarse desde la página en WP.
--->
+<!-- Página de paquetes.-->
+
+
+<?php get_header(); ?>
+
+
+<div class="principal"><!-- INICIA DIV PRINCIPAL-->
+    <div class="cajaDatos">
+        <div class="datos">
+            <div class="nombre">
+                Ximena Monterde<span>Fotógrafa</span>
+            </div>
+            <div class="informacion">
+                5671.2115 / 04455 2756.7111 
+                <a href ="mailto:xmonterde@klique.com.mx"> xmonterde@klique.com.mx</a> 
+                <a href ="mailto:contacto@klique.com.mx"> contacto@klique.com.mx</a>
+                <a href ="http://www.klique.com.mx">www.klique.com.mx</a>
+                <a href="https://www.facebook.com/Kliquefotografia?fref=ts">Facebook: klique</a> 
+            </div>
+            <div class="contacto">
+                <form>
+                    <input class="campos" id="nombre" name="nombre" type=" text" placeholder="nombre">
+                    <input class="campos" id="email" name="email" type="email" placeholder="correo" >
+                    <p><textarea class="campos" id="mensaje" name="mensaje" cols="49" rows="2" placeholder="mensaje"></textarea>
+                    <button class="boton">enviar</button>
+                </form>
+            </div>
+        </div>
+        
+    </div>
+
+<div class="mapa">
+    <iframe width="290" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com.mx/maps?t=m&amp;dg=opt&amp;ie=UTF8&amp;ll=19.344472,-99.16764&amp;spn=0.011196,0.021136&amp;z=16&amp;output=embed"></iframe>
+</div>
+
+
+
+</div><!-- TERMINA DIV PRINCIPAL-->
+
+<?php get_footer();?>
