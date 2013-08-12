c54bc8de28044cc4d127b46edd557272e66a1344
shannonbit <emmanuelle@emprend.me>
2013-08-12 11:28:58 -0500

    #2 QUI001 | Imagen de fondo
    Recibe 3 imágenes. Si se listan más basta con modificar el archivo
    page-quienes-somos.php
    Las imágenes tendrán un ancho optimo de 1366px.
    se recomienda cortar las imágenes antes de enviarlas a la carpeta.

--- wordpress/wp-content/themes/zoom1x/page-quienes-somos.php
+++ wordpress/wp-content/themes/zoom1x/page-quienes-somos.php
@@ -1,30 +1,23 @@
 <!-- Página de Quiénes somos.-->
-	
 <?php get_header(); ?>
-
-
-
+<?php $imagen = array('1', '2', '3'); ?>
+<?php $i = rand(0, 2) ?>
 <!-- inicia imagenes galeria -->
-                <article class="quienesSomos">
-                   
-							<?php 
-								if ( have_posts() ) {
-									while ( have_posts() ) {
-										the_post(); 
-										 the_content();
-										} // end while
-								} // end if
-							?>
-                </article>
-
-                <?php $imagen = array('1', '2', '3'); ?>
-        		<?php $i = rand(0, 2) ?>
-
-                <aside class="imagenQuienes" style="background-image:url(<?php bloginfo('template_directory');?>/img/quienes/<?php echo $imagen[$i]; ?>.jpg); background-size:cover;">
-                </aside>
-                <!-- termina imagene0 galeria -->
-
-
-
-
+    <article class="quienesSomos">
+    	<p>
+    	<?php 
+			if ( have_posts() ) {
+				while ( have_posts() ) {
+					the_post(); 
+					 the_content();
+					} // end while
+			} // end if
+		?>
+		</p>
+    	<img src="<?php bloginfo('template_directory');?>/img/quienes/<?php echo $imagen[$i]; ?>.jpg">
+    </article>
+    <div class="fondoQuienes">
+    	<img src="<?php bloginfo('template_directory');?>/img/quienes/<?php echo $imagen[$i]; ?>.jpg">
+    </div>
+    <!-- termina imagene0 galeria -->
 <?php get_footer();?>
