8342c927f996e09434d5e812e86edae968713f9a
Jorge Olvera <jorge@emprend.me>
2013-10-23 13:21:58 -0500

    Version Final

---
+++ wordpress/wp-content/themes/zoom1x/prueba.php
@@ -0,0 +1,119 @@
+<?php get_header(); ?>
+	<div class="principal">
+			<!-- inicia imagenes galeria -->
+			<div class="galeria">
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+							<div class="imagenIndividual">
+								<a href="<?php echo $src1 ?>" data-lightbox="categoria">
+								<img class="wp-post-image" src="http://www.klique.com.mx/wp-content/uploads/2013/10/PRODUCTO-CORAZON-150x150.jpg" />
+								</a>
+							</div><!-- fin de imagen individual-->
+			</div> <!-- fin de galeria-->	
+	</div>
+
+<?php get_template_part('general/barra', 'categorias'); ?>
+<?php get_footer(); ?>
+
+
+
+
+						
